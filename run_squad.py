4d2c38770b4c66ee5f2a0101fd6bd9a91484ab69
孙宏超 <sunhongchao@ZBMAC-C02WD4GFH.local>
2019-03-20 18:13:55 +0800

    update run_suqad gpu setting

--- run_squad.py
+++ run_squad.py
@@ -1280,4 +1280,5 @@ if __name__ == "__main__":
   flags.mark_flag_as_required("vocab_file")
   flags.mark_flag_as_required("bert_config_file")
   flags.mark_flag_as_required("output_dir")
+  os.environ["CUDA_VISIBLE_DEVICES"] = 1
   tf.app.run()
